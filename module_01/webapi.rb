229922b7f0379ce2f2ad42101122b8ad0658a44f
patrick204nqh <patrick204nqh@gmail.com>
2020-11-23 14:03:57 +0700

    api resource users

---
+++ module_01/webapi.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'json'
+
+users = {
+ 'patrick': { first_name: 'Patrick', last_name: 'Robert', age: '20' },
+ 'kevin': {first_name: 'Kevin', last_name: 'De Bruyne', age: '29' }
+}
+
+get '/' do
+  'Karl Patrick'
+end
+
+get '/users' do
+  users.map { |name, data| data.merge(id: name) }.to_json
+end
